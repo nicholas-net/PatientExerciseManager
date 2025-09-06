10c7b157eab0136b8ddab77cf05abc1d43f65d3e
nicholas-net <cloudvibeclothing@gmail.com>
2025-09-06 12:48:14 -0400

    Initial commit

---
+++ src/main/java/com/nicholascolon/lil/patientexercisemanager/PatientExerciseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nicholascolon.lil.patientexercisemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientExerciseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientExerciseManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicholascolon/lil/patientexercisemanager/PatientExerciseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholascolon.lil.patientexercisemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientExerciseManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
