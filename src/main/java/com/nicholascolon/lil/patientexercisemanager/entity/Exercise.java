b6b0b750e5f7422de376fa281945f32d971e7062
nicholas-net <cloudvibeclothing@gmail.com>
2025-09-09 17:22:26 -0400

    feat: add patient & exercise entity getters and setters

--- src/main/java/com/nicholascolon/lil/patientexercisemanager/entity/Exercise.java
+++ src/main/java/com/nicholascolon/lil/patientexercisemanager/entity/Exercise.java
@@ -21,15 +21,68 @@ public class Exercise {
     @Column(name = "body_part")
     private String bodyPart;
 
+    @Column(name = "image_url")
+    private String imageURL;
+
     protected Exercise() {}
 
-    public Exercise(String exerciseName, int exerciseRepCount, String exerciseBodyPart) {
+    public Exercise(String exerciseName, int exerciseRepCount, String exerciseBodyPart, String exerciseImageURL) {
         this.name = exerciseName;
         this.repCount = exerciseRepCount;
         this.bodyPart = exerciseBodyPart;
+        this.imageURL = exerciseImageURL;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRepCount() {
+        return repCount;
+    }
+
+    public void setRepCount(int repCount) {
+        this.repCount = repCount;
+    }
+
+    public String getBodyPart() {
+        return bodyPart;
+    }
+
+    public void setBodyPart(String bodyPart) {
+        this.bodyPart = bodyPart;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", repCount=" + repCount +
+                ", bodyPart='" + bodyPart + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                '}';
+    }
 
 
 
--- src/main/java/com/nicholascolon/lil/patientexercisemanager/entity/Patient.java
+++ src/main/java/com/nicholascolon/lil/patientexercisemanager/entity/Patient.java
@@ -29,13 +29,45 @@ public class Patient {
         this.lastName = patientLastName;
     }
 
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", age=" + age +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
